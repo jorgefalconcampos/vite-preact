36f8ef56777ac245c1cc7f496a649acb23385fc1
Jorge Falcón <jorgef_campos@outlook.com>
2022-06-28 22:49:57 -0500

    campos dinámicos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,27 @@ import Contenedor from "./components/Contenedor/Contenedor"
 
 function App() {
 
+  // para añadir campos dinámicos a un objeto, hacemos uso de []
+  // dentro de los corchetes ponemos el nombre del campo dinámido
+  
+  let campo = "id"
+  const obj = {
+    nombre: "Jorge",
+    apellido: "Falcón",
+    [campo]: "1234"
+  }
+
+  console.log(obj)
+
+
+
+
+
+
+
+
+
+
   const objStyle = {
     color: "white",
     backgroundColor: "blue",
